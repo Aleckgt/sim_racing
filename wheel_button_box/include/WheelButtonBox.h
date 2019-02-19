aec6b7e445f77ffd197e06a5dc9793300593e23d
aleckgt <aleckgt@gmail.com>
2019-02-19 19:57:30 +0300

    added handler for encoders & buttons

--- wheel_button_box/include/WheelButtonBox.h
+++ wheel_button_box/include/WheelButtonBox.h
@@ -9,10 +9,43 @@
 #define SRC_WHEELBUTTONBOX_H_
 
 #include <Joystick.h>
+#include <Keypad.h>
+#include <Rotary.h>
 
-Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_JOYSTICK, 22, 0,
-false, false, false, false, false, false,
-false, false, false, false, false);
+#define ENCODERS_COUNT 4
+#define BUTTONS_COUNT 12
+#define BUTTONS_ROWS 4
+#define BUTTONS_COLS 3
 
+Joystick_ joystick(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_JOYSTICK, 22, 0,
+		false, false, false, false, false, false, false, false, false, false, false);
+
+
+
+typedef struct {
+	Rotary rotary;
+	byte cw_button;
+	byte ccw_button;
+} Encoder;
+
+Encoder encoders[ENCODERS_COUNT] = {
+	{ Rotary(2, 3), 14, 15},
+	{ Rotary(4, 5), 16, 17},
+	{ Rotary(6, 7), 18, 19},
+	{ Rotary(8, 9), 20, 21}
+};
+
+byte buttons[BUTTONS_ROWS][BUTTONS_COLS] = {
+	{ 2, 3, 4 },
+	{ 5, 6, 7 },
+	{ 8, 9, 10 },
+	{ 11, 12, 13}
+
+};
+
+byte rowPins[BUTTONS_ROWS] = { 10, 16, 14, 15 };
+byte colPins[BUTTONS_COLS] = { A0, A1, A2 };
+
+Keypad buttonBox(makeKeymap(buttons), rowPins, colPins, BUTTONS_ROWS, BUTTONS_COLS);
 
 #endif /* SRC_WHEELBUTTONBOX_H_ */
--- wheel_button_box/src/WheelButtonBox.cpp
+++ wheel_button_box/src/WheelButtonBox.cpp
@@ -7,9 +7,52 @@
 #include <WheelButtonBox.h>
 
 void setup() {
-	Joystick.begin();
+	joystick.begin();
+	pinMode(0, INPUT_PULLUP);
+	pinMode(1, INPUT_PULLUP);
+}
+
+void encoderToButton(char button) {
+	joystick.pressButton(button);
+	delay(50);
+	joystick.releaseButton(button);
 }
 
 void loop() {
-
+	unsigned char res;
+	for (int i = 0; i < ENCODERS_COUNT; i++) {
+		res = encoders[i].rotary.process();
+		if (res == DIR_CCW) {
+			encoderToButton(encoders[i].ccw_button);
+		}
+		if (res == DIR_CW) {
+			encoderToButton(encoders[i].cw_button);
+		}
+	}
+	if (!digitalRead(0)) {
+		joystick.pressButton(1);
+	} else {
+		joystick.releaseButton(1);
+	}
+	if (!digitalRead(1)) {
+		joystick.pressButton(0);
+	} else {
+		joystick.releaseButton(0);
+	}
+	if (buttonBox.getKeys()) {
+		for (int i = 0; i < LIST_MAX; i++) {
+			if (buttonBox.key[i].stateChanged) {
+				switch (buttonBox.key[i].kstate) {
+				case PRESSED:
+				case HOLD:
+					joystick.pressButton(buttonBox.key[i].kchar);
+					break;
+				case RELEASED:
+				case IDLE:
+					joystick.releaseButton(buttonBox.key[i].kchar);
+					break;
+				}
+			}
+		}
+	}
 }
